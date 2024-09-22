78c25ad150973b12061e3f9145a36f9c5fcd5697
Aleksandr Kim <k1mal3xandre@yandex.ru>
2024-09-22 09:46:07 +0800

    1st commit

---
+++ main.py
@@ -0,0 +1,6 @@
+number_of_completed_homework_assignments = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_task = number_of_hours_spent / number_of_completed_homework_assignments
+print('Курс: ' + course_name + ', всего задач:' + str(number_of_completed_homework_assignments) + ', затрачено часов:' + str(number_of_hours_spent) + ', среднее время выполнения ' +  str(time_task) + ' часа.')
+print(f'Курс: {course_name}, всего задач:{number_of_completed_homework_assignments}, затрачено часов:{number_of_hours_spent}, среднее время выполнения {time_task} часа.')
