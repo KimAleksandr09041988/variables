50c4dddd54c75add4b7cbf2d8a8ef5781136a395
Aleksandr Kim <k1mal3xandre@yandex.ru>
2024-09-26 12:24:59 +0800

    hotfix_1 modile_1_5

--- module_1_5.py
+++ module_1_5.py
@@ -2,6 +2,6 @@ immutable_var = 1, 'string', True
 print(immutable_var)
 # immutable_var[2] = False  #Выводится ошибка так как tuple не изменяемый.
 # print(immutable_var)
-mutable_list = ([1, 'string', True], 1, 2)
-mutable_list[0][2] = False
+mutable_list = [1, 'string', True]
+mutable_list[2] = False
 print(mutable_list)
